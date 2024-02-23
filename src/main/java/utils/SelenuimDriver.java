3e8a251e466cf5d6286e919426c5172ac04d6a87
zmarcheva <zlatina_marcheva@abv.bg>
2024-02-23 11:20:08 +0200

    Adding code for disabling geolocation pop-ups

--- src/main/java/utils/SelenuimDriver.java
+++ src/main/java/utils/SelenuimDriver.java
@@ -1,10 +1,15 @@
 package utils;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SelenuimDriver {
 
@@ -14,9 +19,20 @@ public class SelenuimDriver {
 
 
     private SelenuimDriver() {
+
         ChromeOptions opt = new ChromeOptions();
-        //opt.addArguments("incognito");
-        opt.addArguments("--disable-geolocation");
+
+        opt.addArguments("incognito");
+
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        Map<String, Object> profile = new HashMap<String, Object>();
+        Map<String, Object> contentSettings = new HashMap<String, Object>();
+
+        contentSettings.put("geolocation", 1);
+        profile.put("managed_default_content_settings", contentSettings);
+        prefs.put("profile", profile);
+        opt.setExperimentalOption("prefs", prefs);
+
         driver = new ChromeDriver(opt);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
