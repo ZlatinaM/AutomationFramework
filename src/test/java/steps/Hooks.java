30edb4289bb5efabcd767d19be67a3fc0482f958
zmarcheva <zlatina_marcheva@abv.bg>
2024-02-27 12:06:56 +0200

    Adding screenshot in case of failed steps

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,8 +1,14 @@
 package steps;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import utils.SelenuimDriver;
+
+import java.io.File;
 
 public class Hooks {
 
@@ -14,5 +20,14 @@ public class Hooks {
     @After
     public static void tearDown(){
         SelenuimDriver.tearDown();
+    }
+
+    @AfterStep
+    public void takeScreenShot(Scenario scenario){
+        if (scenario.isFailed()){
+            byte[] screenshot = ((TakesScreenshot) SelenuimDriver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
     }
 }
